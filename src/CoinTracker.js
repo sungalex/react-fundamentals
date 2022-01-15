812badc1da7a420032e7173e7822fe198bbeb6b6
sungalex <cloudallera@gmail.com>
2022-01-16 02:35:05 +0900

    Coin Tracker - fetch and list up

--- src/CoinTracker.js
+++ src/CoinTracker.js
@@ -1,7 +1,40 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div>
-      <h1>Coin Tracker</h1>
+      <h1>The Coins! ({coins.length})</h1>
+      {loading ? (
+        <strong>"Loading..."</strong>
+      ) : (
+        <ul>
+          {coins.map((coin, index) => (
+            <li key={index}>
+              {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
+            </li>
+          ))}
+        </ul>
+      )}
+      {/* 
+      <select>
+        {coins.map((coin) => (
+          <option>
+            {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+          </option>
+        ))}
+      </select> 
+      */}
     </div>
   );
 }
