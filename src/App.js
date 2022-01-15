d11b7b9c00057d25d250c51f17967960a84dd068
sungalex <cloudallera@gmail.com>
2022-01-15 23:32:42 +0900

    todo list - part1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import { useState } from "react";
+
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  // console.log(toDo);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  };
+  console.log(toDos);
+
   return (
     <div>
-      <h1>Welcome React Wrold!</h1>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do ..."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
