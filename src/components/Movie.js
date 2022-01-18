0783cd7f6f318721a25d55509348e92a47dc2f29
sungalex <cloudallera@gmail.com>
2022-01-19 01:17:17 +0900

    dynamic routes & useParams

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,13 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-function Movie({ coverImg, title, summary, genres }) {
+function Movie({ id, coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
-      <h2>{title}</h2>
+      <h2>
+        <Link to={`/movie/${id}`}>{title}</Link>
+      </h2>
       <p>{summary}</p>
       <ul>
         {genres.map((g) => (
@@ -16,6 +19,7 @@ function Movie({ coverImg, title, summary, genres }) {
 }
 
 Movie.propTypes = {
+  id: PropTypes.number.isRequired,
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,19 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
+  const { id } = useParams();
+  const getMovieDetail = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    console.log({ json });
+  };
+
+  useEffect(() => {
+    getMovieDetail();
+  }, []);
+
   return <h1>Detail</h1>;
 }
 
