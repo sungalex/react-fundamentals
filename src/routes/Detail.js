7b5ade13698fdb97d2fe628e96ebc643003a7506
sungalex <cloudallera@gmail.com>
2022-01-20 05:07:54 +0900

    setting gh-pages publishing & resolve useEffect missing dependency

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -3,16 +3,18 @@ import { useParams } from "react-router-dom";
 
 function Detail() {
   const { id } = useParams();
-  const getMovieDetail = async () => {
-    const json = await (
-      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
-    ).json();
-    console.log({ json });
-  };
+  const [movie, setMovie] = useState("");
 
   useEffect(() => {
+    const getMovieDetail = async () => {
+      const json = await (
+        await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+      ).json();
+      setMovie(json);
+      console.log({ movie });
+    };
     getMovieDetail();
-  }, []);
+  }, [id, movie]);
 
   return <h1>Detail</h1>;
 }
